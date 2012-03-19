a2bdcb38c54e8b8378fe83e6d96bfd7c6bf2a285
Miguel Rodrigo Pazo Sanchez <miguel.ps19@gmail.com>
2012-03-19 11:40:52 -0500

    Proyecto base para pruebas

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>school test</title>
+    </head>
+    <body>
+        <?php
+         echo "school test";
+        ?>
+    </body>
+</html>
